eebeaedc4b843a9c0dfc066ba714d5a9edd2c7f4
James Aylward <james.michael.aylward@gmail.com>
2024-01-12 19:59:21 +1000

    Added doc and file search

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,21 @@
 use std::error::Error;
+use glob::glob;
 
 pub struct Config {
     pub file: String,
 }
 
 impl Config {
+    /// Builds a Config struct from commandline arguments
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// let config = Config::build(env::args()).unwrap_or_else(|err| {
+    ///     eprintln!("Problem parsing arguments: {err}");
+    ///     process::exit(1);
+    /// });
+    /// ```
     pub fn build(
         mut args: impl Iterator<Item=String>,
     ) -> Result<Config, &'static str> {
@@ -22,5 +33,25 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     println!("{}", config.file);
+    walk_directories().unwrap();
     Ok(())
 }
+
+fn walk_directories() -> Result<(), Box<dyn Error>> {
+    for entry in glob("**/*.rs")? {
+        println!("{}", entry?.display());
+    }
+
+    Ok(())
+}
+
+
+
+
+
+
+
+
+
+
+

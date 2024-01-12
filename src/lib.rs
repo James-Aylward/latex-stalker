af3e0040a1f66bcb6b47c2ba90c130814e998780
James Aylward <james.michael.aylward@gmail.com>
2024-01-12 22:45:18 +1000

    Indexing fn added

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
 #![allow(unused)]
-use std::any::Any;
 use std::error::Error;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::path::PathBuf;
+use std::{thread, time};
 use std::process::Command;
+use std::collections::{hash_map, HashMap};
 use glob::glob;
 
 pub struct Config {
@@ -22,7 +27,7 @@ impl Config {
     ///
     pub fn build(
         mut args: impl Iterator<Item=String>,
-    ) -> Result<Config, &'static str> {
+        ) -> Result<Config, &'static str> {
 
         args.next();
 
@@ -36,14 +41,37 @@ impl Config {
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("{}", config.file);
+    index_files()?;
 
-    let tex_files = glob("**/*.rs")?;
-
-    let mut mupdf_instance = Command::new("mupdf")
+    let mut mupdf_handle = Command::new("mupdf")
         .arg(config.file)
         .spawn()?;
-    println!("{}", mupdf_instance.id());
+
+    //thread::sleep(time::Duration::from_secs(1));
+
+    let refresh_command = Command::new("kill")
+        .arg("-HUP")
+        .arg(format!("{}", mupdf_handle.id()))
+        .output();
 
     Ok(())
+}
+
+fn index_files() -> Result<HashMap<PathBuf, Vec<u8>>, Box<dyn Error>> {
+    let mut footprint: HashMap<PathBuf, Vec<u8>> = HashMap::new();
+
+    for entry in glob("**/*.rs")? {
+        if let Ok(pathbuf) = entry {
+            let mut f = File::open(&pathbuf)?;
+            let mut b = Vec::new();
+            f.read_to_end(&mut b)?;
+            footprint.insert(pathbuf, b);
+        }
+    }
+
+    Ok(footprint)
+}
+
+fn updates_made() -> Result<bool, Box<dyn Error>> {
+    Ok(true)
 }
